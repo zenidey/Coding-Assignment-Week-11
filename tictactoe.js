c9d75f22392730d06733633e67316c4c73017425
zenidey <93101838+zenidey@users.noreply.github.com>
2022-01-27 18:47:52 -0800

    third, it was at this moment when you questioned everything

--- tictactoe.js
+++ tictactoe.js
@@ -1,99 +1,88 @@
-const X_CLASS = 'x'
-const CIRCLE_CLASS = 'circle'
-const WINNING_COMBINATIONS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-]
+// DON'T TOUCH BEYOND THIS POINT ! ----------------------------------------------------
 
-const playerDisplay = document.querySelector('.display-player');
-const cellElements = document.querySelectorAll('[data-cell]');
-const board = document.getElementById('board');
-const winningMessageElement = document.getElementById('winningMessage')
-const restart = document.getElementById('restart');
-const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
+// const playerX = 'x'
+// const playerO = 'circle'
+// const winningCombinations = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6],
+// ]
 
-
-let circleTurn;
-let currentPlayer = 'X';
-
-startGame() 
-
-restart.addEventListener('click', startGame);
-
-function startGame () {
-    circleTurn = false
-    cellElements.forEach(cell => {
-        cell.classList.remove(X_CLASS)
-        cell.classList.remove(CIRCLE_CLASS)
-        cell.removeEventListener('click', handleClick)
-        cell.addEventListener('click', handleClick, { once: true })
-    });
-    setBoardHoverClass()
-    winningMessageElement.classList.remove('show')
-}
-
-function handleClick(e) {
-    const cell = e.target
-    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
-    placeMark(cell, currentClass);
-    if (checkWin(currentClass)) {
-        endGame(false)
-    } else if (isDraw()){
-        endGame(true)
-    } else {
-        swapTurns()
-        setBoardHoverClass()
-    }
-}
-
-function endGame(draw) {
-    if (draw) {
-        winningMessageTextElement.innerText = 'draw!'
-    } else {
-        winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} wins!`
-    }
-    winningMessageElement.classList.add('show')
-}
-
-function isDraw() {
-    return [...cellElements].every(cell => {
-        return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS);
-    })
-}
-
-function placeMark(cell, currentClass) {
-    cell.classList.add(currentClass);
-}
-
-function swapTurns() {
-    circleTurn = !circleTurn
-    playerDisplay.classList.remove(`player${currentPlayer}`);
-    currentPlayer = currentPlayer === 'X' ? 'O' : "X";
-    playerDisplay.innerText = currentPlayer;
-    playerDisplay.classList.add(`player${currentPlayer}`);
-}
+// const restart = document.getElementById('restart');
+// const cellElements = document.querySelectorAll('[data-cell]'); // selecting all of the different cells
+// const winningMessageElement = document.getElementById('winningMessage') //
+// const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
+// const playerDisplay = document.querySelector('.display-player');
+// let currentPlayer = 'X';
+// let circleTurn;
 
 
-function setBoardHoverClass() {
-    board.classList.remove(X_CLASS)
-    board.classList.remove(CIRCLE_CLASS)
-    if (circleTurn) {
-        board.classList.add(CIRCLE_CLASS)
-    } else {
-        board.classList.add(X_CLASS)
-    }
-}
+// startGame() 
 
-function checkWin(currentClass) {
-    return WINNING_COMBINATIONS.some(combination => {
-        return combination.every(index => {
-            return cellElements[index].classList.contains(currentClass)
-        })
-    })
-}
+// restart.addEventListener('click', startGame);
+
+// function startGame () {
+//     circleTurn = false
+//     cellElements.forEach(cell => {      // looping through the cells         
+//         cell.classList.remove(playerX);
+//         cell.classList.remove(playerO);
+//         cell.removeEventListener('click', whenClicked); // syntax: element.addEventListener(event, function, useCapture); the third parameter is a boolean value
+//         cell.addEventListener('click', whenClicked, { once: true });   // every time we click on a cell we want to render an X or an O, {once:true} ensures it only renders once
+//     });
+//     winningMessageElement.classList.remove('show')
+// }
+
+// function whenClicked(event) {
+//     const cell = event.target
+//     const currentPlayer = circleTurn ? playerO : playerX // current class = if it's circle return circle in not x
+//     mark(cell, currentPlayer);
+//     if (checkWin(currentPlayer)) {
+//         endGame(false)
+//     } else if (isDraw()){
+//         endGame(true)
+//     } else {
+//         swapTurns()
+//     }
+// }
+
+// function mark(cell, currentPlayer) {
+//     cell.classList.add(currentPlayer);  // add the the mark according to who's turn it is
+// }
+
+// function checkWin(currentPlayer) {
+//     return winningCombinations.some(combination => {    // return true if any of the winning combinations are met
+//         return combination.every(index => {     // checking if all the index have the same mark (ex: X, X, X)
+//             return cellElements[index].classList.contains(currentPlayer);
+//         })
+//     })
+// }
+
+// function endGame(draw) {
+//     if (draw) {
+//         winningMessageTextElement.innerText = 'draw!'
+//     } else {
+//         winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} wins!`
+//     }
+//     winningMessageElement.classList.add('show')
+// }
+
+// function isDraw() {
+//     return [...cellElements].every(cell => {
+//         return cell.classList.contains(playerX) || cell.classList.contains(playerO);
+//     })
+// }
+
+// function swapTurns() {
+//     circleTurn = !circleTurn        // swapping between X and O turns
+//     playerDisplay.classList.remove(`player${currentPlayer}`);
+//     currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+//     playerDisplay.innerText = currentPlayer;
+//     playerDisplay.classList.add(`player${currentPlayer}`);
+
+// }
+
